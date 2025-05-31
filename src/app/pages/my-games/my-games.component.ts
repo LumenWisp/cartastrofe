d4f640cdc9c8deef7dbdbbc729be44825d8cf1f2
Yasushis <viniciusmiura1@gmail.com>
2025-05-31 15:44:18 -0300

    Add: searchbar faltando estilização

--- src/app/pages/my-games/my-games.component.ts
+++ src/app/pages/my-games/my-games.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { PanelGameComponent } from '../../components/panel-game/panel-game.component';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-my-games',
-  imports: [PanelGameComponent],
+  imports: [PanelGameComponent, IconFieldModule, InputIconModule, InputTextModule],
   templateUrl: './my-games.component.html',
   styleUrl: './my-games.component.css'
 })
