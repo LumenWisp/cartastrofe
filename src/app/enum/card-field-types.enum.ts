ad5d66196207e9f3efbc1f28dd218db3b1305bb4
Yasushis <viniciusmiura1@gmail.com>
2025-08-02 11:11:46 -0300

    ADD: pagina simples para a criação de layout

---
+++ src/app/enum/card-field-types.enum.ts
@@ -0,0 +1,4 @@
+export enum CardFieldTypes {
+  IMAGE = 'IMAGE',
+  TEXT = 'TEXT',
+}
--- src/app/pages/my-layouts/create-layout/create-layout.component.ts
+++ src/app/pages/my-layouts/create-layout/create-layout.component.ts
@@ -1,13 +1,43 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { InputTextModule } from 'primeng/inputtext';
+import { CdkDrag, CdkDropList } from '@angular/cdk/drag-drop';
+import { MenuModule } from 'primeng/menu';
+import { CardFieldTypes } from '../../../enum/card-field-types.enum';
 
 @Component({
   selector: 'app-create-layout',
-  imports: [RouterOutlet, InputTextModule],
+  imports: [CdkDrag, CdkDropList, MenuModule],
   templateUrl: './create-layout.component.html',
-  styleUrl: './create-layout.component.css'
+  styleUrl: './create-layout.component.css',
 })
 export class CreateLayoutComponent {
-  value: string = ''
+  // Configuração do menu lateral
+  menuItems = [
+    {
+      label: 'Adicionar campos',
+      items: [
+        {
+          label: 'Campo de Texto',
+          icon: 'pi pi-file',
+          command: () => this.addField(CardFieldTypes.TEXT),
+        },
+        {
+          label: 'Campo de Imagem',
+          icon: 'pi pi-image',
+          command: () => this.addField(CardFieldTypes.IMAGE),
+        },
+      ],
+    },
+  ];
+
+  // Campos da carta baseado no ENUM
+  cardFields: { type: CardFieldTypes }[] = [];
+
+  // Função para adicionar um bloco tipado no cardFields
+  addField(type: CardFieldTypes.TEXT | CardFieldTypes.IMAGE) {
+    this.cardFields.push({ type });
+  }
+
+  consolar() {
+    console.log('consolei')
+  }
 }
