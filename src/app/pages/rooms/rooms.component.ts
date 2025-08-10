e3c6ebddb949d137f956b9d8b07ce4f78aa5a28e
Julio Estrela <julioantunesestrela13@gmail.com>
2025-08-10 02:14:24 -0300

    CTF-15 - Internacionalização | Rooms

--- src/app/pages/rooms/rooms.component.ts
+++ src/app/pages/rooms/rooms.component.ts
@@ -4,10 +4,11 @@ import { ButtonModule } from 'primeng/button';
 import { RouterModule } from '@angular/router';
 import { UserEntity } from '../../types/user';
 import { UserService } from '../../services/user-service.service';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-rooms',
-  imports: [PanelModule, ButtonModule, RouterModule],
+  imports: [PanelModule, ButtonModule, RouterModule, TranslatePipe],
   templateUrl: './rooms.component.html',
   styleUrl: './rooms.component.css'
 })
